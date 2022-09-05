af7189674d090950aa9e0e32db9dbb48ad852219
Yuri Freire <yuriifreire@hotmail.com>
2022-09-05 15:13:02 -0300

    Update settings.py
    remove secret key

--- minitwt/settings.py
+++ minitwt/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'ytft6em6i2efddh0y!n7%w^um3k0i)&!5(f__u!$e55n45&h14'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
